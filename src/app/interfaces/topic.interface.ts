ecb154906d94ba382d2e86ab821b2877809ca738
Sarah Ashraf <35216590+saraanafady@users.noreply.github.com>
2025-05-09 15:57:32 +0300

    Update topic.interface with category

--- src/app/interfaces/topic.interface.ts
+++ src/app/interfaces/topic.interface.ts
@@ -1,14 +1,18 @@
 export interface Topic {
+  title: any;
   _id?: string;
 
   name: {
     en: string;
-    ar?: string; 
+    ar?: string;
   };
-  slug: string;
+  slug: {
+    en: string;
+    ar?: string;
+  };
   description: {
     en: string;
-    ar?: string; 
+    ar?: string;
   };
   thumbnailImgUrl: string;
   availableLanguages: string[];
@@ -16,4 +20,5 @@ export interface Topic {
   courseCount: number;
   createdAt?: string;
   updatedAt?: string;
+  category: string;
 }
